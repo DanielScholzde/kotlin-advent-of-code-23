046ba26d1859d6ac39e5e1fb03ec5167944e3fe4
Daniel Scholz <danielsch@gmx.net>
2023-12-03 13:39:33 +0100

    Advent of Code - Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+
+    // part 1
+
+    input.sumOf { row ->
+        val firstDigit = row.first { it in '0'..'9' }
+        val lastDigit = row.last { it in '0'..'9' }
+        (firstDigit.toString() + lastDigit).toInt()
+    }.println()
+
+    // part 2
+
+    val numbersAsMap = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        .mapIndexed { index, numStr -> numStr to index + 1 }.toMap() +
+            (1..9).associateBy { it.toString() }
+
+    val regex = Regex(numbersAsMap.keys.joinToString("|"))
+
+    input.sumOf { row ->
+        val first = regex.find(row)!!.value
+
+        var last = "-"
+        var i = -1
+        do {
+            val matchResult = regex.find(row, i + 1) ?: break
+            last = matchResult.value
+            i = matchResult.range.first
+        } while (true)
+
+        (numbersAsMap[first].toString() + numbersAsMap[last]).toInt()
+
+    }.println()
+
 }
